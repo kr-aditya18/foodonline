98facb979a4e96218e39b355d8f270770dcd0c92
kr-aditya18 <vermaadiitya13678@gmail.com>
2026-03-04 17:06:31 +0530

    Built vendor and customer dashboards

--- vendor/urls.py
+++ vendor/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path
-
+from django.urls import path,include
+from accounts import views as AccountViews
+from . import views
 urlpatterns = [
+    path('',AccountViews.vendordashboard,name='vendor'),
+    path('profile/',views.vprofile,name='vprofile'),
     
 ]
--- vendor/views.py
+++ vendor/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+
+# from .models import Vendor
+# # Create your views here.
+# def vprofile(request):
+#     context = {
+#         'vendor': vendor
+#     }
+#     return render(request,'vendor/vprofile.html')
+
+
+from django.shortcuts import render
+from .models import Vendor
+
+def vprofile(request):
+
+    return render(request, 'vendor/vprofile.html')
