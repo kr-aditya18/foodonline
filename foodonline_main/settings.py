62faa1adce1bc19fa29c62c228271968529977fe
kr-aditya18 <vermaadiitya13678@gmail.com>
2026-04-15 15:45:25 +0530

    add Dockerfile and render config

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -36,7 +36,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # Lesson 209: Custom vendor order middleware
     'orders.middleware.VendorOrderMiddleware',
 ]
 
